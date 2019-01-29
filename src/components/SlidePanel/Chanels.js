fa84743760a95740e73afd0b71546ddff48d2f6b
VietCuong <levietcuong0297@gmail.com>
2019-01-30 00:38:14 +0700

    add modal, add chanel

--- src/components/SlidePanel/Chanels.js
+++ src/components/SlidePanel/Chanels.js
@@ -1,22 +1,104 @@
 import React, { Component } from 'react';
-import { Menu, Icon } from 'semantic-ui-react';
+import { Menu, Icon, Modal, Form, Input, Button } from 'semantic-ui-react';
+import firebase from '../../firebase';
+import { connect } from 'react-redux';
 
 class Chanels extends Component {
+    state = {
+        chanels: [],
+        modal: false,
+        chanelName: '',
+        chanelDetail: '',
+        chanelRefs: firebase.database().ref('chanels'),
+    }
+
+    handleChange = evt => this.setState({ [evt.target.name]: evt.target.value });
+
+    openModal = () => this.setState({modal: true});
+
+    closeModal = () => this.setState({modal: false});
+
+    handleSubmit = evt => { 
+        evt.preventDefault();
+        if(this.isFormEmpty(this.state)){
+            this.addChanel();
+        }
+    };
+
+    addChanel = () => {
+        const { chanelDetail, chanelName, chanelRefs } = this.state;
+        const { user } = this.props;
+        const key = chanelRefs.push().key;
+
+        const newChanel = {
+            id: key,
+            name: chanelName,
+            detail: chanelDetail,
+            createBy: {
+                name: user.displayName,
+                avatar: user.photoURL
+            }
+        }
+        
+        chanelRefs
+            .child(key)
+            .update(newChanel)
+            .then(()=> {
+                console.log("added chanel ");
+                this.setState({ chanelName: '', chanelDetail: ''});
+                this.closeModal();
+            })
+            .catch(err => console.log(err));
+    }
+
+    isFormEmpty = ({chanelName, chanelDetail}) => chanelName && chanelDetail;
+
     render() {
-        const chanels = [];
+        const { chanels, modal } = this.state;
         return (
-            <Menu.Menu style={{ paddingBottom: '2em' }}>
-                <Menu.Item>
-                    <span>
-                        <Icon name='exchange'/>
-                        CHANELS
-                    </span>
-                    ({chanels.length})
-                    <Icon name='add'/>
-                </Menu.Item>
-            </Menu.Menu>
+            <React.Fragment>
+                {/* Menu chanels */}
+                <Menu.Menu style={{ paddingBottom: '2em' }}>
+                    <Menu.Item>
+                        <span>
+                            <Icon name='exchange'/>
+                            CHANELS
+                        </span>
+                        ({chanels.length})
+                        <Icon name='add' onClick={ this.openModal }/>
+                    </Menu.Item>
+                </Menu.Menu>
+                {/* Add modal chanel */}
+                <Modal basic open={modal} onClose={ this.closeModal }>
+                    <Modal.Header>Add a Chanels</Modal.Header>
+                    <Modal.Content>
+                        <Form onSubmit={this.handleSubmit}>
+                            <Form.Field>
+                                <Input fluid label='Name of Chanel' name='chanelName' onChange={this.handleChange}/>
+                            </Form.Field>
+                            <Form.Field>
+                                <Input fluid label='Chanel detail' name='chanelDetail' onChange={this.handleChange}/>
+                            </Form.Field>
+                        </Form>   
+                    </Modal.Content>
+                    <Modal.Actions>
+                        <Button color='green' inverted onClick={ this.handleSubmit }>
+                            <Icon name='checkmark' />ADD
+                        </Button>
+                        <Button color='red' inverted onClick={ this.closeModal }>
+                            <Icon name='remove' />CANCEL
+                        </Button>
+                    </Modal.Actions>
+                </Modal>
+            </React.Fragment>
         );
     }
 }
 
-export default Chanels;
+const mapStateToProps = state => {
+    return {
+        user: state.user.currentUser
+    }
+}
+
+export default connect(mapStateToProps)(Chanels);
